a77b129e779abe9d851ede50720fde5d78597a1a
DawidGlazik <dawid-glazik@wp.pl>
2025-03-22 19:18:50 +0100

    Signing a pdf file + little cleanup

--- gui.py
+++ gui.py
@@ -1,34 +1,60 @@
 import tkinter as tk
-from tkinter import filedialog, messagebox
 import threading
+from tkinter import filedialog
 
+from usbDetector import *
+from signFile import *
 
-from usbDetector import monitor_usb
 
 def select_file():
-    file_path = filedialog.askopenfilename(filetypes=[("PDF files", "*.pdf")])
+    file_path = filedialog.askopenfilename(filetypes=[("Pliki PDF", "*.pdf")])
     if file_path:
         entry_file.delete(0, tk.END)
         entry_file.insert(0, file_path)
+
 
 def sign_document():
     file_path = entry_file.get()
     pin = entry_pin.get()
     if not file_path or not pin:
         messagebox.showerror("Błąd", "Wybierz plik PDF i wpisz PIN.")
+        return
+
+    if not encrypted_pkey:
+        messagebox.showerror("Błąd", "Nie wykryto klucza prywatnego.")
+        return
+
+    decrypted_pkey = decrypt_key(pin)
+    if decrypted_pkey is None:
+        return
+
+    hash = generate_hash(file_path)
+    if hash is None:
+        return
+
+    signature = create_signature(hash, decrypted_pkey)
+    if signature is None:
+        return
+
+    if not bond_signature_and_pdf(file_path, signature):
+        return
+
 
 def verify_signature():
     file_path = entry_file.get()
     if not file_path:
         messagebox.showerror("Błąd", "Wybierz plik PDF do weryfikacji.")
 
+
 def show_signing_view():
     verification_screen.pack_forget()
     signing_screen.pack(pady=10)
 
+
 def show_verification_view():
     signing_screen.pack_forget()
     verification_screen.pack(pady=10)
+
 
 def signing_view(root):
     signing_screen = tk.Frame(root)
@@ -43,6 +69,7 @@ def signing_view(root):
     
     return signing_screen, entry_file, entry_pin
 
+
 def verification_view(root):
     verification_screen = tk.Frame(root)
     tk.Label(verification_screen, text="Plik PDF:").pack(pady=5)
@@ -50,6 +77,7 @@ def verification_view(root):
     tk.Button(verification_screen, text="Zweryfikuj podpis", command=verify_signature).pack(pady=5)
     
     return verification_screen
+
 
 def navigation_view(root):
     nav_frame = tk.Frame(root)
